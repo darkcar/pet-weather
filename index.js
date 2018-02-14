55510d504ffcea0e220e1f30782e42cc963b9cd2
Xuegang Wang <darkcar88@gmail.com>
2018-02-14 14:57:17 -0600

    remove errors

--- index.js
+++ index.js
@@ -8,18 +8,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-var pg = require('pg');
-var app = express();
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
